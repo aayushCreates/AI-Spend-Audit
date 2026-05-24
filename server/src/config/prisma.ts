d2bcfcdbad8c0d8cff3745539e485af0a819f626
AyushAJS <jangidayush89@gmail.com>
2026-05-24 13:59:26 +0530

    audit types and prisma and server init

---
+++ server/src/config/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter
+});
