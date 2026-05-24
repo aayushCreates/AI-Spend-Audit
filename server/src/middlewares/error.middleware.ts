970d0eb7b584f9819ec166bd822c55dfb247ac63
AyushAJS <jangidayush89@gmail.com>
2026-05-24 14:05:59 +0530

    Pricing data and error middleware is added

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+  ) {
+    super(message);
+    this.message = message;
+  }
+}
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  console.error("Unhandled error:", err);
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+}
